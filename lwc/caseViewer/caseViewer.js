ca0d4a91ac123648cf46f525879b9b5eb14f6ffd
Cristalle Ganter <31865314+CristalleGanter@users.noreply.github.com>
2026-06-01 22:00:34 -0400

    Update caseViewer.js

--- lwc/caseViewer/caseViewer.js
+++ lwc/caseViewer/caseViewer.js
@@ -1,5 +1,8 @@
-import { LightningElement, api, track } from 'lwc';
-import GET_CASES from '@salesforce/apex/caseViewerController.getCases';
+import { LightningElement, api, track, wire } from 'lwc';
+import getCases from '@salesforce/apex/caseViewerController.getCases';
+import closeCases from '@salesforce/apex/caseViewerController.closeCases';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 
 const columns = [
     {label: 'Subject', fieldName: 'Subject'},
@@ -8,30 +11,62 @@ const columns = [
     {label: 'Duration (Days)', fieldName: 'Duration_in_Days__c'},
 ];
 
+
 export default class CaseViewer extends LightningElement {
 
     @api recordId;
     columns = columns;
-    caseRecords;
+    wiredCases;
     hasCases = false;
-
-    connectedCallback(){
-        this.getCases();
-
-    }
-
-    async getCases(){
-        GET_CASES({relatedAccountId: this.recordId})
-        .then(result => {
-            this.caseRecords = result;
-            if (this.caseRecords.length > 0 ){
+    selectedCases;
+    variant = 'success';
+   
+@wire(getCases, {relatedAccountId: '$recordId'})
+wiredCases(response){
+       if (response.data) {
+            this.wiredCases = response.data;
+            if (this.wiredCases.length > 0) {
                 this.hasCases = true;
             }
-            console.log('these are the case records: '+ this.caseRecords);
-        }) 
-        .catch(error => {
-            console.log('Error during getCases: '+ error.body.message);
+            
+            this.error = undefined;
+            console.log('cases found: '+ this.wiredCases);
+        } else if (response.error) {
+            this.error = response.error;
+            this.wiredCases = undefined;
+            console.log('error getting cases: '+ this.error)
+        }
+};
+
+    handleCloseCases(){ 
+        console.log('in the case close method. '+ JSON.stringify(this.selectedCases));
+        closeCases ({openCases: this.selectedCases})
+        .then(result => {
+                console.log('case closed.');
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'Success!',
+                    message: 'Case Closed',
+                    variant: this.variant
+                    
+                }));
+                    
         })
+        .catch(error => {
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error',
+                message: 'Please contact your administrator',
+                variant: 'error'
+            }));
+            console.log("close case error: "+ error.body.message);
+        });
+       
     }
+
+    
+    handleRowSelection(event){
+            this.selectedCases = event.detail.selectedRows;
+            console.log('selected cases: '+ JSON.stringify(this.selectedCases));
+    }
+
 
 }
